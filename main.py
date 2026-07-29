95db8760a7940c28cc00fa736027d9b714e886b8
Gangala Tanishka <tanishkagangala@gmail.com>
2026-07-30 00:51:57 +0530

    configure production CORS

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:4173",
         "http://127.0.0.1:4173",
+        "https://aves-dashboard.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
